301f77fd49c06a776cfe7d1ed554f9b3c9a2c6e6
“Sajeda <sajedakader.qa@gmail.com>
2023-02-25 15:56:46 -0500

    commit third

--- Project2/cypress/e2e/sessionTwo.cy.js
+++ Project2/cypress/e2e/sessionTwo.cy.js
@@ -8,6 +8,7 @@ describe('tasks page', () => {
 
   it('should display the page title', () => {
     cy.visit('http://127.0.0.1:5173');
+    cy.get('h1').should('have.length', 1);
     cy.get('h1').contains('My Cypress Course Tasks');
   });
 
