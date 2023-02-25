9292e3270333694ae28777e798adfe54ccf6967b
“Sajeda <sajedakader.qa@gmail.com>
2023-02-25 15:44:49 -0500

    second commit

--- Project2/cypress/e2e/sessionTwo.cy.js
+++ Project2/cypress/e2e/sessionTwo.cy.js
@@ -1,6 +1,14 @@
+/// <reference types="Cypress" />
+
 describe('tasks page', () => {
   it('should render the main image', () => {
     cy.visit('http://127.0.0.1:5173');
+    cy.get('.main-header img');
+  });
+
+  it('should display the page title', () => {
+    cy.visit('http://127.0.0.1:5173');
+    cy.get('h1').contains('My Cypress Course Tasks');
   });
 
 });
