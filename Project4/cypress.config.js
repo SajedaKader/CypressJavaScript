47a294cddad6080f41e72f07bc81804df8745301
“Sajeda <sajedakader.qa@gmail.com>
2023-03-01 12:28:29 -0500

    Tasks

--- Project4/cypress.config.js
+++ Project4/cypress.config.js
@@ -7,6 +7,13 @@ export default defineConfig({
     baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        seedDataBase(filename) {
+          // Run your NodeJs code
+          //e.g., edit a file here 
+          return filename;
+        }
+      });
     },
   },
 });
