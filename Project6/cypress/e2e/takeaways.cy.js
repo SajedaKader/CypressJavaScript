459e15c8946b2f9f079fbc6cc4ddb8e841355f67
“Sajeda <sajedakader.qa@gmail.com>
2023-03-02 13:44:08 -0500

    testing api with test request

--- Project6/cypress/e2e/takeaways.cy.js
+++ Project6/cypress/e2e/takeaways.cy.js
@@ -1,8 +1,16 @@
 /// <reference types="Cypress" />
 
-describe('Takeaways', () => {
-  it('should display a list of fetched takeaways', () => {
-    cy.visit('/');
-    cy.get('[data-cy="takeaway-item"]').should('have.length', 2);
+describe('Newsletter', () => {
+  beforeEach(() => {
+    cy.task('seedDatabase');
   });
+  it('should display a success message'), () => E
+  cy.intercept('POST', '/newsletter*', {
+    status: 201
+  }).as('subscribe'); // intercept ar
+  cy.visit('/');
+  cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+  cy.get('[data-cy="newsletter-submit"]').click();
+  cy.wait('@subscribe');
+  cy.contains('Thanks for signing up');
 });
