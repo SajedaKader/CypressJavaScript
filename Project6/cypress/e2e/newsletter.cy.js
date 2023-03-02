68d61ceef11c9080886ffeeda906a87177246336
“Sajeda <sajedakader.qa@gmail.com>
2023-03-02 13:17:34 -0500

    creating test database & seeding the testdata before every test & another database dependent test

---
+++ Project6/cypress/e2e/newsletter.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe('Newsletter', () => {
+    beforeEach(() => {
+        cy.task('seedDatabase');
+    });
+    it('should display a success message', () => {
+        cy.visit('/');
+        cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+        cy.get('[data-cy="newsletter-submit"]').click();
+        cy.contains('Thanks for signing up');
+    });
+});
